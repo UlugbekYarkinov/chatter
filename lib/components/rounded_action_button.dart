5207420a124383e87f65a4a4c7cb3ce6505e3d87
UlugbekYarkinov <yarkinovulugbek@gmail.com>
2023-04-12 09:56:12 +0500

    code refactored

---
+++ lib/components/rounded_action_button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class RoundedActionButton extends StatelessWidget {
+  const RoundedActionButton({
+    Key? key,
+    required this.color,
+    required this.onPressed,
+    required this.label,
+  }) : super(key: key);
+
+  final Color color;
+  final void Function() onPressed;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        color: color,
+        borderRadius: BorderRadius.circular(30.0),
+        elevation: 5.0,
+        child: MaterialButton(
+          onPressed: onPressed,
+          minWidth: 200.0,
+          height: 42.0,
+          child: Text(
+            label,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,3 +1,4 @@
+import 'package:chatter/components/rounded_action_button.dart';
 import 'package:chatter/screens/login_screen.dart';
 import 'package:chatter/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
@@ -81,41 +82,20 @@ class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProvider
             const SizedBox(
               height: 48.0,
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                elevation: 5.0,
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, LoginScreen.id);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: const Text(
-                    'Log In',
-                  ),
-                ),
-              ),
+
+            RoundedActionButton(
+              color: Colors.lightBlueAccent,
+              onPressed: () {
+                Navigator.pushNamed(context, LoginScreen.id);
+              },
+              label: 'Log In',
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, RegistrationScreen.id);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: const Text(
-                    'Register',
-                  ),
-                ),
-              ),
+            RoundedActionButton(
+              color: Colors.blueAccent,
+              onPressed: () {
+                Navigator.pushNamed(context, RegistrationScreen.id);
+              },
+              label: 'Register',
             ),
           ],
         ),
