edcb69b504a1be944a7981c54f2e94a5d1192552
UlugbekYarkinov <yarkinovulugbek@gmail.com>
2023-04-21 16:52:11 +0500

    loading spinner added

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -3,6 +3,7 @@ import 'package:chatter/constants.dart';
 import 'package:chatter/screens/chat_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -16,6 +17,7 @@ class _LoginScreenState extends State<LoginScreen> {
   final _auth = FirebaseAuth.instance;
   late String email;
   late String password;
+  bool showSpinner = false;
   bool registrationError = false;
 
   @override
@@ -30,70 +32,81 @@ class _LoginScreenState extends State<LoginScreen> {
         ),
       ),
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: SizedBox(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: SizedBox(
+                  height: 200.0,
+                  child: Image.asset('images/logo.png'),
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              textAlign: TextAlign.center,
-              onChanged: (value) {
-                email = value;
-              },
-              decoration: inputTextFieldDecoration.copyWith(hintText: 'Enter your email'),
-            ),
-            const SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              textAlign: TextAlign.center,
-              obscureText: true,
-              onChanged: (value) {
-                password = value;
-              },
-              decoration: inputTextFieldDecoration.copyWith(hintText: 'Enter your password'),
-            ),
-            const SizedBox(
-              height: 12.0,
-            ),
-            Text(
-              registrationError ? "Either email or password is badly formatted" : " ",
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                color: Colors.red,
+              const SizedBox(
+                height: 48.0,
               ),
-            ),
-            const SizedBox(
-              height: 12.0,
-            ),
-            RoundedActionButton(
-              color: Colors.lightBlueAccent,
-              onPressed: () async{
-                try {
-                  await _auth.signInWithEmailAndPassword(email: email, password: password);
-                  if(mounted) {
-                    Navigator.pushNamed(context, ChatScreen.id);
+              TextField(
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  email = value;
+                },
+                decoration: inputTextFieldDecoration.copyWith(hintText: 'Enter your email'),
+              ),
+              const SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                textAlign: TextAlign.center,
+                obscureText: true,
+                onChanged: (value) {
+                  password = value;
+                },
+                decoration: inputTextFieldDecoration.copyWith(hintText: 'Enter your password'),
+              ),
+              const SizedBox(
+                height: 12.0,
+              ),
+              Text(
+                registrationError ? "Either email or password is badly formatted" : " ",
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  color: Colors.red,
+                ),
+              ),
+              const SizedBox(
+                height: 12.0,
+              ),
+              RoundedActionButton(
+                color: Colors.lightBlueAccent,
+                onPressed: () async{
+                  try {
+                    setState(() {
+                      showSpinner = true;
+                    });
+
+                    await _auth.signInWithEmailAndPassword(email: email, password: password);
+                    if(mounted) {
+                      Navigator.pushNamed(context, ChatScreen.id);
+                    }
+
+                    setState(() {
+                      showSpinner = false;
+                    });
+                  } catch(e) {
+                    setState(() {
+                      registrationError = true;
+                    });
                   }
-                } catch(e) {
-                  setState(() {
-                    registrationError = true;
-                  });
-                }
-              },
-              label: 'Log In',
-            ),
-          ],
+                },
+                label: 'Log In',
+              ),
+            ],
+          ),
         ),
       ),
     );
